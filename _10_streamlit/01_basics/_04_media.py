983f06478180c2eb8a22fac7c09b02034d0bfa91
SooYeon Lee <leso0326@gmail.com>
2026-06-09 17:03:43 +0900

    20260609_stremlit print media

---
+++ _10_streamlit/01_basics/_04_media.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Media - Image')
+st.image("../02_data/princess.jpg", caption="Princess Don't Cry")
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWW73eNzGtTiwA-_foQcOq15u8rCOVihwvqg&s", caption="Everybody Xuck you / web img")
+
+
+
+
